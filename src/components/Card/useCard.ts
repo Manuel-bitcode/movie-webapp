2e3604ee1858da226546bbb5e8a98d9324fda3d3
Manuel Martinez <mmartinez@comparaonline.com>
2025-10-24 00:33:27 -0500

    feat(docker): add config alias

--- src/components/Card/useCard.ts
+++ src/components/Card/useCard.ts
@@ -1,9 +1,18 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useCard = () => {
   const [isLiked, setIsLiked] = useState(false)
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/movies`)
+      const data = await response.json()
+      console.log('data', data)
+    }
+    fetchMovie()
+  }, [])
 
   return { isLiked, setIsLiked }
 }
